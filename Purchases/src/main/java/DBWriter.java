449be292d5cab114603e747ea186591474548bcb
HeeJun You <71749263+hjyou07@users.noreply.github.com>
2021-03-23 15:37:29 -0700

    POSTman request successful, and the microservice writes to DB

--- Purchases/src/main/java/DBWriter.java
+++ Purchases/src/main/java/DBWriter.java
@@ -1,45 +1,97 @@
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class DBWriter implements Runnable {
+  private Connection conn;
+  private String QUEUE_NAME = "dbwriter";
+  private String EXCHANGE_NAME = "micro";
+
+  public DBWriter(Connection conn, String QUEUE_NAME, String EXCHANGE_NAME) {
+    this.conn = conn;
+    this.QUEUE_NAME = QUEUE_NAME;
+    this.EXCHANGE_NAME = EXCHANGE_NAME;
+  }
 
   @Override
   public void run() {
-    // TODO 4: Lift this up to the Purchase microservice (separate project)
-    // now try creating the purchase POJO object from the json string
-    Purchase purchase = readRequestBody(reqBody);
+    Channel channel = null;
+   try {
+     channel = conn.createChannel();
+     // TODO 3: lift this off by creating a dummy channel in PurchasesMicroService
+     channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+     /* queueDeclare() parameters
+      queue - the name of the queue
+      durable - true if we are declaring a durable queue (the queue will survive a server restart)
+      exclusive - true if we are declaring an exclusive queue (restricted to this connection)
+      autoDelete - true if we are declaring an autodelete queue (server will delete it when no longer in use)
+      arguments - other properties (construction arguments) for the queue
+     */
+     channel.queueDeclare(QUEUE_NAME, false, true, false, null);
+     // TODO 3.1: this too
+     channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "");
+     // max one message per receiver
+     channel.basicQos(1);
 
-    if (!isRequestValid(purchase)) {
-      res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      res.getWriter().write("Bad requestBody");
-      return;
+     ChannelConsumer consumer = new ChannelConsumer(channel);
+     // I can pass in a DeliverCallback and it is preferred over Consumer for a lambda-oriented syntax, but I'm just exploring.
+     // also, auto-ack is off with this overloaded signature. auto-cak is "fire and forget"
+     channel.basicConsume(QUEUE_NAME, consumer);
+   } catch (IOException e) {
+     e.printStackTrace();
+   }
+   // TODO: closing channel got me a bunch of errors, where and when should I close it?
+
+  }
+
+  public class ChannelConsumer extends DefaultConsumer {
+
+    public ChannelConsumer(Channel channel) {
+      super(channel);
     }
 
-    try {
-      PurchaseDao dao = new PurchaseDao();
-      dao.createPurchaseInDB(purchase);
-      res.setStatus(
-          HttpServletResponse.SC_CREATED); // if url valid, status code: 201 Write Successful
-      res.getWriter().write(String.format("echoing the request body: %s", purchase.items));
-    } catch (SQLException e) {
-      res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      res.getWriter().write("There was a problem writing to the DB");
-    } catch (Exception e) {
-      System.err.println("PurchaseDao can't be created");
-      e.printStackTrace();
+    /**
+     * Called when a basic.deliver is received for this consumer. Equivalent to functional interface DeliverCallback's handle()
+     * @param consumerTag the consumer tag associated with the consumer
+     * @param envelope packaging data for the message
+     * @param properties content header data for the message
+     * @param body the message body (opaque, client specific bye array)
+     * @throws IOException
+     */
+    @Override
+    public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties,
+        byte[] body) throws IOException {
+      String message = new String(body, "UTF-8");
+      // TODO 1: Lift this up to the Purchase microservice (separate project)
+      // now try creating the purchase POJO object from the json string
+      Purchase purchase = readRequestBody(message);
+      // TODO 2: Send ack once write to the db succeeds.
+      try {
+        PurchaseDao dao = new PurchaseDao();
+        dao.createPurchaseInDB(purchase);
+        // b: false indicates to ack just the supplied delivery tag
+        getChannel().basicAck(envelope.getDeliveryTag(), false);
+      } catch (SQLException e) {
+        e.printStackTrace();
+        // b: true indicates requeueing
+        getChannel().basicReject(envelope.getDeliveryTag(), true);
+      }
+    }
+
+    private Purchase readRequestBody(String reqBody) {
+      String[] parsed = reqBody.split("#");
+      for (String s : parsed) { System.out.println(s); }
+      String items = parsed[0];
+      int storeID = Integer.valueOf(parsed[1]);
+      int custID = Integer.valueOf(parsed[2]);
+      String purchaseDate = parsed[3];
+      return new Purchase(storeID, custID, purchaseDate, items);
     }
   }
 
-  private boolean isRequestValid(Purchase purchase) {
-    // TODO: implement a validation for the ProtoPurchase POJO created by request body
-    return true;
-  }
-
-  private Purchase readRequestBody(String reqBody) {
-    String[] parsed = reqBody.split("#");
-    for (String s : parsed) { System.out.println(s); }
-    int storeID = Integer.valueOf(parsed[1]);
-    int custID = Integer.valueOf(parsed[2]);
-    String purchaseDate = parsed[3];
-    return new Purchase(storeID, custID, purchaseDate, reqBody);
-  }
 }
--- Purchases/src/main/java/PurchasesMicroService.java
+++ Purchases/src/main/java/PurchasesMicroService.java
@@ -1,10 +1,8 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class PurchasesMicroService {
   private static final String QUEUE_NAME = "dbwriter";
@@ -15,14 +13,19 @@ public class PurchasesMicroService {
     factory.setHost("localhost"); // TODO: use properties file
     ExecutorService dbWriterPool = Executors.newFixedThreadPool(60); // match the dbcp poolSize
 
-    try (Connection conn = factory.newConnection(dbWriterPool);) {
+    Connection conn = null;
+    try {
+      conn = factory.newConnection(dbWriterPool);
       for (int i=0; i < 65; i++) {
-        dbWriterPool.execute(new DBWriter());
+        dbWriterPool.execute(new DBWriter(conn, QUEUE_NAME, EXCHANGE_NAME));
       }
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw e;
     }
     // wrap up, tells the ExecutorService not to accept any more tasks to run
-    dbWriterPool.shutdown();
+    // dbWriterPool.shutdown();
     // blocks the calling thread until all threads are idle and no more work is waiting in the queue
-    dbWriterPool.awaitTermination(10, TimeUnit.SECONDS);
+    // dbWriterPool.awaitTermination(10, TimeUnit.SECONDS);
   }
 }
